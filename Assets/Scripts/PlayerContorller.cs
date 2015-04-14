21fec3c254615b8ecd72d4b3e6c7f2b73aa81798
minkowang <for.js221@gmail.com>
2015-04-14 19:31:33 +0800

    新增两个状态

---
+++ Assets/Scripts/PlayerContorller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContorller : MonoBehaviour {
+
+	// Use this for initialization
+    private Animator anim;
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", Mathf.Abs(speed));
+	}
+}
