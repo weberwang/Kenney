8a3b42afbb5ea9cd458664ebe0a045617353d503
minkowang <for.js221@gmail.com>
2015-04-15 18:06:05 +0800

    完成角色状态动画

---
+++ Assets/Scripts/ActionContorller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionContorller : MonoBehaviour {
+
+	// Use this for initialization
+    private Animator anim;
+    private Rigidbody2D body2d;
+
+    private bool jump = false;
+    private bool shouldJump = false;
+	void Start () {
+        anim = GetComponent<Animator>();
+        body2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        if (jump && shouldJump)
+        {
+            anim.SetTrigger("jump");
+            body2d.AddForce(Vector2.up * body2d.mass * 200);
+            shouldJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "floor")
+        {
+            anim.SetTrigger("stand");
+            shouldJump = true;
+        }
+    }
+}
--- Assets/Scripts/PlayerContorller.cs
+++ Assets/Scripts/PlayerContorller.cs
@@ -4,14 +4,10 @@ using System.Collections;
 public class PlayerContorller : MonoBehaviour {
 
 	// Use this for initialization
-    private Animator anim;
 	void Start () {
-        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float speed = Input.GetAxis("Horizontal");
-        anim.SetFloat("speed", Mathf.Abs(speed));
 	}
 }
